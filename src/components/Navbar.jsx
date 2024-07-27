1f976f55aa7c99ba38f0747c4b02ccad475fb88c
AkhilaePacken <akhila@epacken.com>
2024-07-27 14:06:00 +0400

    projects updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,14 @@
-import { FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { FaSquareThreads } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa";
 import { CONTACT_LINKS } from "../constants";
-import { BsTwitterX } from "react-icons/bs";
-import { RiEmotionNormalLine, RiTwitterXLine } from "react-icons/ri";
+import { RiTwitterXLine } from "react-icons/ri";
 import { MdMailOutline } from "react-icons/md";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <h1 className="text-3xl  tracking-tight bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text  text-transparent lg:mt-16">
+        <h1 className="text-3xl  tracking-tight bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text  text-transparent ">
           {"<Aks/>"}
         </h1>
       </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,10 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import portfolio from "../assets/developer.jpg";
+import medetuitePic from "../assets/projects/project-5.png";
+import eDronaPic from "../assets/projects/edronaPic.webp";
+import ePackenPic from "../assets/projects/ePackenPic.png";
+import nestPic from "../assets/projects/Nest.webp";
+import foodMaPic from "../assets/projects/foodmazone.webp";
+import smartfolioPic from "../assets/projects/smarfolio.svg";
 
 export const HERO_CONTENT = `I'm Akhila K S. Software Engineer, React Native Enthusiast, Hybrid Mobile App Developer (iOS & Android).
 
@@ -88,32 +91,125 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "CRM Application",
-    image: project1,
+    title: "ePacken CRM Application",
+    image: ePackenPic,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "When the client specifies their needs, they simply submit a short online form detailing the property and the services they require. Our AI will then shortlist experienced, prequalified, and vetted contractors tailored to the specific needs and project requirements. Clients can compare quotes and proposals from these qualified contractors and hire the one that best suits their project.",
+    technologies: [
+      "React Native",
+      "TypeScript",
+      "React frameworks",
+      "Hooks",
+      "HOC",
+      "data drilling",
+      "middle ware approaches",
+      "Redux",
+      "Redux Toolkit",
+      "GraphQL",
+      "Rest API",
+      "Axios",
+      "micro services",
+      "async-storage",
+      "react-native-vector-icons",
+      "jest",
+      "yup and react-hook-form validation",
+      "react-i18next",
+      "react-native-biometrics",
+      "react-native-gifted-charts for Graphs",
+      "react-native-reanimated",
+      "react-native-reanimated-carousel",
+      "react-native-render-html",
+      "react-native-signature-canvas",
+      "react-native-svg",
+      "react-native-streetview",
+      "react-native-webview",
+    ],
+    link: "https://www.crm-demo.epacken.com/",
+  },
+
+  {
+    title: "Medituite",
+    image: medetuitePic,
+    description:
+      "Medetuit is a medical learning platform offering high-quality, interactive medical learning videos adhering to the new medical curriculum. Medetuit is committed to provide the most comprehensive visual learning experience, freeing the medical students from the world of bulky books & interminable lectures. They envision revolutionizing the way medical learning is globally perceived, thereby breezing change into how future medical students approach the course as a whole.",
+    technologies: [
+      "React Native",
+      "Redux",
+      "JavaScript",
+      "axios",
+      "react-native-video-player",
+      "@vimeo/player",
+      "Rest API",
+      "Zoom meet and G-meet SDK",
+    ],
+    link: "https://medetuit.com/",
+  },
+
+  {
+    title: "e-Drona",
+    image: eDronaPic,
+    description:
+      "This mobile applications developed for educational institutions provide comprehensive classroom management services, including live and recorded online class sessions. Implemented a video player for recorded classes, integrated Google Meet and Zoom SDKs for live classes, and integrated PayPal, Razorpay, and in-app purchases as payment gateways, enhancing the functionality and user experience of mobile applications.",
+    technologies: [
+      "React Native",
+      "Redux",
+      "JavaScript",
+      "axios",
+      "react-native-video-player",
+      "@vimeo/player",
+      "Rest API",
+    ],
+    link: "https://e-drona.in/",
   },
   {
     title: "Nest Matrimony",
-    image: project2,
+    image: nestPic,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Firebase"],
+      "Nest Matrimony , a matrimonial service provider from Kerala, approached us to create a unique web application. We crafted a highly engaging web application which was developed using Laravel and MySQL to ensure security and a flawless backend as the application stores and handles personal data. Today, Nest has around 1.5 lakh customers and is one of the leading matrimonial service providers in Kerala.",
+    technologies: [
+      "React Native",
+      "Redux",
+      "Firebase",
+      "react-native-phone-call",
+      "react-native-reanimated",
+    ],
+    link: "https://apps.apple.com/in/app/nest-matrimony/id1486299424",
+  },
+  {
+    title: "Foodmazone",
+    image: foodMaPic,
+    description:
+      "Foodmazone make grocery ordering experience an effortless venture with our custom-made suite of grocery delivery mobile app. The app has simple and fast checkout, powered by a fully-secured gateway to ensure high-security payments. We made the app dynamic with the settings management system to enable our customers to make the necessary changes in the app in lesser time.",
+    technologies: [
+      "React Native",
+      "Redux",
+      "Firebase",
+      "react-native-map",
+      "react-native-maps-directions",
+    ],
+    link: "https://play.google.com/store/apps/details?id=com.shop.foodmazone&hl=en",
+  },
+  {
+    title: "Smarfolio By Geojit",
+    image: smartfolioPic,
+    description:
+      "This app will guide you to Smartfolios, a hassle-free, data-driven platform where equity investments meet intelligence. Smartfolios offers expertly curated baskets of stocks, enabling you to invest based on your unique investment appetite and outlook, ultimately helping you achieve your financial goals.",
+    technologies: [
+      "React Native",
+      "Redux",
+      "Firebase",
+      "react native animation",
+      "react native svg images",
+      "",
+    ],
+    link: "https://smartfolios.geojit.com/#/login",
   },
   {
     title: "Portfolio Website",
-    image: project3,
+    image: portfolio,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React"],
-  },
-  {
-    title: "Medituite",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS"],
+    technologies: ["Tailwind CSS", "React", "Framer Motion", "VITE"],
   },
 ];
 
